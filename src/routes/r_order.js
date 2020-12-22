ed744f9a6d56a43cc58a5d419d0fc6d47cdb3a89
rizqonmaulana <rizqonmaulana5@gmail.com>
2020-12-23 06:07:56 +0800

    update : add deleteByIdModel for order history

--- src/routes/r_order.js
+++ src/routes/r_order.js
@@ -2,13 +2,17 @@ const router = require('express').Router()
 const {
   getOrder,
   getOrderDetail,
+  getOrderHistory,
   postOrder,
-  deleteOrder
+  deleteOrder,
+  deleteOrderDetailById
 } = require('../controller/c_order')
 
 router.get('/:customerId', getOrder)
 router.get('/invoice/:invoice', getOrderDetail)
+router.get('/history/:customerId', getOrderHistory)
 router.post('/', postOrder)
 router.delete('/:id', deleteOrder)
+router.delete('/history/:id', deleteOrderDetailById)
 
 module.exports = router
