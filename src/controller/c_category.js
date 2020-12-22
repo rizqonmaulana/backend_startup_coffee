b8eb87261a181023f1ba1b4726d7bd9030332056
rizqonmaulana <rizqonmaulana5@gmail.com>
2020-12-23 05:58:28 +0800

    feature : add pagination on category endpoint

--- src/controller/c_category.js
+++ src/controller/c_category.js
@@ -1,16 +1,55 @@
-const { getProductByCategoryModel } = require('../model/m_category')
+const {
+  getProductByCategoryModel,
+  getCategoryCountModel
+} = require('../model/m_category')
 const helper = require('../helper/response')
+const qs = require('querystring')
 
 module.exports = {
   getProductByCategory: async (request, response) => {
     try {
       const { categoryName } = request.params
-      const result = await getProductByCategoryModel(categoryName)
+      let { page, limit } = request.query
+      page = parseInt(page)
+      limit = parseInt(limit)
+
+      const totalData = await getCategoryCountModel(categoryName)
+      const totalPage = Math.ceil(totalData / limit)
+      const offset = page * limit - limit
+      const prevLink =
+        page > 1
+          ? qs.stringify({ ...request.query, ...{ page: page - 1 } })
+          : null
+      const nextLink =
+        page < totalPage
+          ? qs.stringify({ ...request.query, ...{ page: page + 1 } })
+          : null // page=...&limit=...
+      console.log(request.query)
+      console.log(qs.stringify(request.query))
+      const pageInfo = {
+        page,
+        totalPage,
+        limit,
+        totalData,
+        nextLink:
+          nextLink &&
+          `http://localhost:3000/category/${categoryName}?${nextLink}`,
+        prevLink:
+          prevLink &&
+          `http://localhost:3000/category/${categoryName}?${prevLink}`
+      }
+
+      const result = await getProductByCategoryModel(
+        categoryName,
+        limit,
+        offset
+      )
       return helper.response(
         response,
         200,
         `Success get product by category : ${categoryName}`,
-        result
+        result,
+        pageInfo
       )
     } catch (error) {
       return helper.response(response, 400, 'Bad Request', error)
--- src/model/m_category.js
+++ src/model/m_category.js
@@ -1,12 +1,24 @@
 const connection = require('../config/mysql')
 
 module.exports = {
-  getProductByCategoryModel: (categoryName) => {
+  getProductByCategoryModel: (categoryName, limit, offset) => {
+    return new Promise((resolve, reject) => {
+      console.log(
+        connection.query(
+          `SELECT * FROM product JOIN category ON product.category_id = category.category_id WHERE category.category_name = '${categoryName}' LIMIT ${limit} OFFSET ${offset}`,
+          (error, result) => {
+            !error ? resolve(result) : reject(new Error(error))
+          }
+        )
+      )
+    })
+  },
+  getCategoryCountModel: (categoryName) => {
     return new Promise((resolve, reject) => {
       connection.query(
-        `SELECT * FROM product JOIN category ON product.category_id = category.category_id WHERE category.category_name = '${categoryName}'`,
+        `SELECT COUNT(*) AS total FROM category JOIN product on category.category_id = product.category_id WHERE category_name = '${categoryName}'`,
         (error, result) => {
-          !error ? resolve(result) : reject(new Error(error))
+          !error ? resolve(result[0].total) : reject(new Error(error))
         }
       )
     })
